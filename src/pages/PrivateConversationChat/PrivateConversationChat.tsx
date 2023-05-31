67e8fafd93bdbe7e815054396054a03ebd2c7746
ChristoferMendes <christoferluizdsm@gmail.com>
2023-05-31 11:25:30 -0300

    feat: add other user banner to private conversation chat page

--- src/pages/PrivateConversationChat/PrivateConversationChat.tsx
+++ src/pages/PrivateConversationChat/PrivateConversationChat.tsx
@@ -2,12 +2,17 @@ import { Chat } from '../../components/Chat';
 import { ChatBanner } from '../../components/ChatBanner';
 import { ChatContainer } from '../../components/ChatContainer';
 import { MessageInput } from '../../components/MessageInput';
+import { useOtherUserOnPrivateConversation } from '../../store/useOtherUserOnPrivateConversation';
 
 export function PrivateConversationChat() {
+  const { otherUser } = useOtherUserOnPrivateConversation();
+
   return (
     <div className="flex-1">
       <ChatContainer>
-        <ChatBanner />
+        {otherUser && (
+          <ChatBanner imageUrl={otherUser.avatar_url ?? ''} name={otherUser.first_name} />
+        )}
         <Chat />
         <MessageInput />
       </ChatContainer>
